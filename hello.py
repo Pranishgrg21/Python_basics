496092ec4437a56b9e83754394072b01bdc85c03
Pranishgrg21 <gpranish989@gmail.com>
2026-04-16 18:54:32 +0545

    first commit

---
+++ hello.py
@@ -0,0 +1,16 @@
+print("Hello World!")
+
+name = "Ram"
+print(name)
+
+# arguments
+"""Positional agrument"""
+print("Hello", name) 
+
+
+# Hello Ram. How are you doing?
+
+print("Hello "+name+" . How are you doing")
+
+print("Hello", name, "How are you doing")
+
---
+++ input.py
@@ -0,0 +1,6 @@
+# print("Enter a name:", end=" ")
+# name = input()
+# print(name)
+
+name = input("Enter a name: ")
+print(name)
